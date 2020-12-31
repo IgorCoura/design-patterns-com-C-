b9999f30d83d4fb512b4048d27f2a2e67705ad4f
Igor Coura <igor-coura@hotmail.com>
2020-12-31 01:25:51 -0300

    Padronizando

--- src/Infra.Repository.Detran/VerificadorDebitosRepository/DetranSPVerificadorDebitosRepository.cs
+++ src/Infra.Repository.Detran/VerificadorDebitosRepository/DetranSPVerificadorDebitosRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DesignPatternSamples.Infra.Repository.Detran
 {
-    public class DetranSPVerificadorDebitosRepository : IDetranVerificadorDebitosRepository
+    public class DetranSPVerificadorDebitosRepository : DetranVerificadorDebitosRepositoryCrawlerBase
     {
         private readonly ILogger _Logger;
 
@@ -15,10 +15,16 @@ namespace DesignPatternSamples.Infra.Repository.Detran
             _Logger = logger;
         }
 
-        public Task<IEnumerable<DebitoVeiculo>> ConsultarDebitos(Veiculo veiculo)
+        protected override Task<IEnumerable<DebitoVeiculo>> PadronizarResultado(string html)
+        {
+            _Logger.LogDebug($"Padronizando o Resultado {html}.");
+            return Task.FromResult<IEnumerable<DebitoVeiculo>>(new List<DebitoVeiculo>() { new DebitoVeiculo() });
+        }
+
+        protected override Task<string> RealizarAcesso(Veiculo veiculo)
         {
             _Logger.LogDebug($"Consultando débitos do veículo placa {veiculo.Placa} para o estado de SP.");
-            return Task.FromResult<IEnumerable<DebitoVeiculo>>(new List<DebitoVeiculo>() { new DebitoVeiculo() });
+            return Task.FromResult("CONTEUDO DO SITE DO DETRAN/SP");
         }
     }
 }
